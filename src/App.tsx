df2cb769418a4cdd2c7bfad2e81777ab16d01e22
ShoheiTakei <st.jwd196@gmail.com>
2024-09-08 16:38:19 +0900

    settings tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import '../src/index.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="flex flex-col min-h-screen bg-gray-900 text-white">
+      <main className="flex-1 p-8 mb-20">
+        <header className="flex justify-between items-center mb-10">
+          <h1 className="text-4xl font-bold">Music App</h1>
+        </header>
+        <section>
+          <h2 className="text-2xl font-semibold mb-5">Search for a song</h2>
+        </section>
+      </main>
     </div>
   );
 }
-
-export default App;
